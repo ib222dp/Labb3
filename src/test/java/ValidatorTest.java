318cbb12c25b46db6ddf4c422724163934751b9b
ib222dp <ib222dp@student.lnu.se>
2024-11-11 16:14:15 +0100

    Added maven plugins

--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ValidatorTest {
 
